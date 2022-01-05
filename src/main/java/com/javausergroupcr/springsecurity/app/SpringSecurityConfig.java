18ce1c86af15fd46cb09a7633a30d5657001001d
= <=>
2022-01-04 18:56:38 -0600

    feat: Implementation spring security with JDBC

---
+++ src/main/java/com/javausergroupcr/springsecurity/app/SpringSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.javausergroupcr.springsecurity.app;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.javausergroupcr.springsecurity.app.auth.handler.LoginSuccessHandler;
+
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private LoginSuccessHandler successHandler;
+
+	@Autowired
+	private DataSource dataSource;
+
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/list").permitAll()
+		.anyRequest().authenticated()
+		.and()
+			.formLogin()
+				.successHandler(successHandler)
+				.loginPage("/login")
+			.permitAll()
+		.and()
+		.logout().permitAll()
+		.and().
+		exceptionHandling().accessDeniedPage("/error_403");
+
+	}
+
+	@Autowired
+	public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception {
+
+		build.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder)
+				.usersByUsernameQuery("select username, password, enabled from users where username=?")
+				.authoritiesByUsernameQuery(
+						"select u.username, a.authority from authorities a inner join users u on (a.user_id=u.id) where u.username=?");
+	}
+}
